9476d3162a0ce49d37bf8d43eec8f2cd398c7092
GustinBridou <lecornecgustin@gmail.com>
2025-11-26 10:47:36 +0100

    Add files via upload

---
+++ scriptEntrees.sql
@@ -0,0 +1,20 @@
+drop DATABASE IF EXISTS entrees_bdd;
+
+create DATABASE entrees_bdd;
+
+use entrees_bdd;
+
+CREATE TABLE entrees (
+ 
+)Engine = InnoDB;
+
+INSERT 
+
+
+USE mysql;
+DELETE FROM db WHERE User='testeur';
+
+
+GRANT 
+
+FLUSH PRIVILEGES;
